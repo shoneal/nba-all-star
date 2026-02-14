dd62fa3a63f2f0984cf2b1d864fff073bcdf37cf
Roman Pigunov <jerome.malone@yandex.ru>
2026-02-14 17:25:38 +0300

    1

---
+++ scripts/players.js
@@ -0,0 +1,66 @@
+const players = {
+  west: [
+    {
+      firstName: "Nikola",
+      lastName: "Jokic",
+      team: "den",
+      number: 15,
+    },
+    {
+      firstName: "Shai",
+      lastName: "Gilgeous-Alexander",
+      team: "okc",
+      number: 2,
+    },
+    {
+      firstName: "Victor",
+      lastName: "Wembanyama",
+      team: "sas",
+      number: 1,
+    },
+    {
+      firstName: "Alperen",
+      lastName: "Sengun",
+      team: "hou",
+      number: 28,
+    },
+    {
+      firstName: "Stephen",
+      lastName: "Curry",
+      team: "gsw",
+      number: 30,
+    },
+  ],
+  east: [
+    {
+      firstName: "Giannis",
+      lastName: "Antetokounmpo",
+      team: "mil",
+      number: 34,
+    },
+    {
+      firstName: "Cade",
+      lastName: "Cunningham",
+      team: "det",
+      number: 2,
+    },
+    {
+      firstName: "Jalen",
+      lastName: "Johnson",
+      team: "atl",
+      number: 1,
+    },
+    {
+      firstName: "Jaylen",
+      lastName: "Brown",
+      team: "bos",
+      number: 7,
+    },
+    {
+      firstName: "Jalen",
+      lastName: "Brunson",
+      team: "nyk",
+      number: 11,
+    },
+  ],
+};
